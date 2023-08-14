858b92b2e694679f3fb7e5124d7c961e672d5424
obaraelijah <elijahobara357@gmail.com>
2023-08-14 17:22:39 +0300

    feat: add bookmodel and error handling middleware

--- server/server.js
+++ server/server.js
@@ -1,6 +1,16 @@
 require("dotenv").config();
 const express = require("express");
 const connectToDB=require("./database/db")
+const ErrorsMiddleware = require("./middleware/errorMiddleware");
+const LibraryError = require("./utils/libraryError");
+
+process.on("uncaughtException", (error) => {
+    console.log("Uncaught Exception..... 💣 🔥 stopping the server....");
+    console.log(error.name, error.message);
+
+    process.exit(1);
+});
+
 
 const app = express()
 
@@ -16,8 +26,24 @@ app.get('/test', (req, res) => {
     });
 });
 
-app.listen(PORT, 
+app.all("*", (req, res, next) => {
+    next(
+        new LibraryError(`Can't find ${req.originalUrl} on this server!`, 404)
+    );
+});
+app.use(ErrorsMiddleware);
+
+
+const server = app.listen(PORT, 
     console.log(
         `Server running in ${process.env.NODE_ENV} mode on port ${PORT}`
     )
 );
+
+process.on("unhandledRejection", (error) => {
+    console.log("Unhandled Rejection..... 💣 🔥 stopping the server....");
+    console.log(error.name, error.message);
+    server.close(() => {
+        process.exit(1);
+    });
+});

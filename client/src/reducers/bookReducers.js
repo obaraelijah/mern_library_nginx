17b83195ae99b72ffce1cdcf455535f3e15c30a7
obaraelijah <elijahobara357@gmail.com>
2023-08-16 21:24:39 +0300

    feat: add actions types and reducers

--- client/src/reducers/bookReducers.js
+++ client/src/reducers/bookReducers.js
@@ -1,6 +1,19 @@
-export const bookListReducer = (state={books:[]}, action => {
+import {
+    BOOK_LIST_FAIL,
+    BOOK_LIST_REQUEST,
+    BOOK_LIST_SUCCESS,
+ } from "../actions/types";
+
+export const bookListReducer = (state = { books: [] }, action) => {
     switch (action.type) {
-        default:
-            return state
+       case BOOK_LIST_REQUEST:
+          return { loading: true, books: [] };
+       case BOOK_LIST_SUCCESS:
+          return { loading: false, books: action.payload };
+       case BOOK_LIST_FAIL:
+          return { loading: false, error: action.payload };
+       default:
+          return state;
     }
-});
+ };
+ 
